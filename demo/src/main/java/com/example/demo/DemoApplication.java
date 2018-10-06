16faf328a9f8ba92f9b3f1f8eab04db8ea58eea3
XiaoXin <920812213@qq.com>
2018-10-07 00:09:06 +0800

    构建Spring Cloud 配置服务器

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,12 +5,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.stereotype.Component;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableCaching
 @CacheNamespace(implementation=(com.example.demo.config.MybatisRedisCache.class))
 @MapperScan("com.example.demo.mapper")
+@EnableConfigServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
